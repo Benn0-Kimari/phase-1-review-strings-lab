78fde3d654a804221f03dfb192bba7b12663f91a
benard <47benn0@gmail.com>
2025-03-04 11:13:22 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = `Grace Hopper`;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!!`;
+
+// Define excitedWelcomeMessage in all uppercase with an exclamation 
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`
